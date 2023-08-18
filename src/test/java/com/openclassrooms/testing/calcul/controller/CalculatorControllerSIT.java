07292781d2b9687376e7e7f47dee491a445738b9
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-08-18 18:37:33 +0200

    test integration system of CalculatorController

---
+++ src/test/java/com/openclassrooms/testing/calcul/controller/CalculatorControllerSIT.java
@@ -0,0 +1,14 @@
+package com.openclassrooms.testing.calcul.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorControllerSIT {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
